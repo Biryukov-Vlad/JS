0230645046a0d3174ef11a3f4029a6955de5080f
KorobovA <annavesssna@icloud.com>
2019-08-14 20:45:11 +0400

    занятие

---
+++ script.js
@@ -0,0 +1,46 @@
+// var text_1 = document.getElementsByClassName("text_1")[0];
+// var text_2 = document.getElementsByClassName("text_2")[0];
+// var text_3 = document.getElementsByClassName("text_3")[0];
+// var text_4 = document.getElementsByClassName("text_4")[0];
+// text_1.classList.toggle("text_red")
+// text_1.innerHTML = "dgfdfdd";
+
+// text_1.innerHTML = "<p>srhshshshsadhsdhsdhsdhsdh</p>";
+
+// text_1.classList.add("text_black", "aegwsg");
+// for (var i = 0; i < document.body.childNodes.length; i++) {
+//     console.log(document.body.childNodes[i]);
+// }
+
+// // text.style.color = "red";
+// // text.style.background = "black";
+// // text.style.width = "50%";
+
+// // text.style.height = "100px";
+// // text.style.display = "flex";
+
+// setTimeout(function test() {
+//     text_1.style.color = "red";
+//     text_1.style.background = "black";
+//     text_1.style.width = "50%";
+//     text_1.style.height = "100px";
+//     text_1.style.display = "flex";
+//     text_1.style.top = "100px";
+// }, 2000)
+
+var box_1 = document.getElementsByClassName("box_1")[0];
+var box_2 = document.getElementsByClassName("box_2")[0];
+var box_3 = document.getElementsByClassName("box_3")[0];
+var box_4 = document.getElementsByClassName("box_4")[0];
+
+// setTimeout(function test() {
+//     box_1.classList.toggle("box-active_1 ")
+//     // box_1.style.top = "100px"
+//     // box_1.style.left = "100px"
+// }, 2000)
+
+box_1.classList.toggle("box_active_1");
+box_2.classList.toggle("box_active_2");
+box_3.classList.toggle("box_active_3");
+box_4.classList.toggle("box_active_4");
+// box_4.style.top = "0px"
